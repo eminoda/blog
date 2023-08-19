e0d803e4b6c61ebf11a13728e9bcc92cac03cd54
史兴豪 <245978782@qq.com>
2023-08-19 10:29:44 +0800

    feat: 目录滚动定位

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare module 'nonce-str' {
+    function nonce(bit: number): string;
+    export = nonce;
+}
--- src/app/[...post]/category.tsx
+++ src/app/[...post]/category.tsx
@@ -1,25 +1,39 @@
 "use client";
 import { useEffect, useState } from "react";
+import React, { MouseEvent } from "react";
 import { useRef } from "react";
+import nonce from "nonce-str";
+
 interface CategoryNode {
   tag: string;
   title: string;
   active?: boolean;
+  id: string;
   children: CategoryNode[];
 }
 
 function CategoryNodes(props: { nodes: CategoryNode[]; deep: number }) {
+  // TODO
+  const scrollTo = (e: MouseEvent<HTMLElement>, id: string) => {
+    const $title = document.querySelector("#" + id);
+    if ($title) {
+      console.log(`document.querySelector("#" + ${id})`, $title, $title!.getBoundingClientRect().top);
+      window.scrollTo({
+        top: $title!.getBoundingClientRect().top + window.scrollY - 80,
+      });
+    }
+  };
   return (
-    <ol className={"space-y-2 ml-" + props.deep * 3}>
+    <ol className={props.deep == 0 ? "space-y-2 " : "space-y-2 ml-4"}>
       {props.nodes.map((item, index) => {
         return (
           <li key={index}>
-            <a href="" className="hover:underline decoration-gray-300 underline-offset-2 cursor-pointer">
+            <span onClick={(e) => scrollTo(e, item.id)} className="hover:underline decoration-gray-300 underline-offset-2 cursor-pointer">
               {item.tag == "h2" ? "📚" : "🏷️" + (index + 1).toString() + ". "}
               <span className={item.active ? "text-red-400" : ""}>
-                {item.title}---{item.active}
+                {item.title}---{item.id}
               </span>
-            </a>
+            </span>
             {item.children.length > 0 && <CategoryNodes nodes={item.children} deep={props.deep + 1} />}
           </li>
         );
@@ -39,13 +53,15 @@ export default function Category() {
   // 构建目录结构
   const buildCategoryTree = (nodes: HTMLCollection): CategoryNode[] => {
     const levels = ["h2", "h3"];
-    const list = Array.from(nodes)
-      .filter((item) => {
-        return levels.includes(item.nodeName.toLowerCase());
-      })
-      .map((item) => {
-        return { tag: item.nodeName.toLowerCase(), title: item.innerHTML };
-      });
+    const list = [];
+    for (let i = 0; i < nodes.length; i++) {
+      const item = nodes[i];
+      if (levels.includes(item.nodeName.toLowerCase())) {
+        const id = "title_" + nonce(6);
+        item.setAttribute("id", id);
+        list.push({ id, tag: item.nodeName.toLowerCase(), title: item.innerHTML });
+      }
+    }
 
     setCategoryFlat(list);
 
@@ -69,23 +85,19 @@ export default function Category() {
 
   // 激活目录
   useEffect(() => {
-    let count = -1;
-    const deepWalk = (list: CategoryNode[]) => {
+    let count = 0;
+    const deepWalk = (list: CategoryNode[]): boolean => {
       for (let i = 0; i < list.length; i++) {
-        count++;
-        if (count == titleIndex) {
-          list[i].active = true;
-          count = Infinity;
-        } else {
-          if (list[i].children.length > 0) {
-            deepWalk(list[i].children);
-          }
-          list[i].active = count == titleIndex;
+        // 当前元素
+        list[i].active = count == titleIndex;
+        if (list[i].children.length > 0) {
+          count++;
+          deepWalk(list[i].children);
         }
+        count++;
       }
     };
     deepWalk(tree);
-    console.log(tree);
   }, [titleIndex, tree]);
 
   // 计算当前标题
@@ -94,24 +106,18 @@ export default function Category() {
     const $titleNodes = Array.from($rootNode?.children!).filter((node) => {
       return ["h2", "h3"].includes(node.nodeName.toLowerCase());
     });
-    let lastTop = Infinity;
     for (let i = 0; i < $titleNodes.length; i++) {
       const top = $titleNodes[i].getBoundingClientRect().top!;
 
       // 距离顶部距离
-      if (top > 0 && top < 150) {
-        // 当前最小标题
-        if (top <= lastTop) {
-          lastTop = top;
+      if (0 < top && top < 360) {
+        if (titleIndex >= 0) {
           setTitleIndex(i);
           break;
         }
-      } else if (top > 150) {
-        if (top <= lastTop) {
-          lastTop = top;
-          setTitleIndex(i);
-          break;
-        }
+      } else if (top >= 360) {
+        setTitleIndex(i);
+        break;
       }
     }
   }, [categoryFlat, offsetY]);
@@ -141,9 +147,6 @@ export default function Category() {
   return (
     <>
       <div className="pt-16 w-80 bg-sky-950 text-gray-300 p-6 fixed right-0 top-0 z-10 h-full">
-        {treeDeep}
-        {treeDeepIndex}
-        {titleIndex}
         <CategoryNodes nodes={tree} deep={0} />
       </div>
     </>
