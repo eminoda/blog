a9a597b2ebd8479f30a803d132ff402c92c77a60
史兴豪 <245978782@qq.com>
2023-08-19 10:35:01 +0800

    style: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,40 +8,42 @@ export default function Home() {
   return (
     <div className="bg-gray-100 pb-10">
       <Header />
-      <Banner />
-      {/* 主内容区 */}
-      <div className="flex justify-center items-start px-10 mt-10 space-x-20">
-        {/* 个人介绍 */}
-        <div className="sticky top-14 w-80 rounded-md shadow-lg p-6 bg-white flex items-center flex-col">
-          <Image className="rounded-full" src="/images/logo.jpg" width={70} height={70} alt="logo" />
-          <div className="text-base font-bold mt-4">前端雨爸</div>
-          <div className="text-sm mt-2">Frontend Developer</div>
-          <div className="flex space-x-6 mt-4">
-            <Image className="cursor-pointer" src="/images/icon/github.png" width={26} height={26} alt="logo" />
-            <Image className="cursor-pointer" src="/images/icon/mp.png" width={26} height={26} alt="logo" />
-            <Image className="cursor-pointer" src="/images/icon/toutiao.png" width={26} height={26} alt="logo" />
+      <div className="pt-[44px]">
+        <Banner />
+        {/* 主内容区 */}
+        <div className="flex justify-center items-start px-10 mt-10 space-x-20">
+          {/* 个人介绍 */}
+          <div className="sticky top-14 w-80 rounded-md shadow-lg p-6 bg-white flex items-center flex-col">
+            <Image className="rounded-full" src="/images/logo.jpg" width={70} height={70} alt="logo" />
+            <div className="text-base font-bold mt-4">前端雨爸</div>
+            <div className="text-sm mt-2">Frontend Developer</div>
+            <div className="flex space-x-6 mt-4">
+              <Image className="cursor-pointer" src="/images/icon/github.png" width={26} height={26} alt="logo" />
+              <Image className="cursor-pointer" src="/images/icon/mp.png" width={26} height={26} alt="logo" />
+              <Image className="cursor-pointer" src="/images/icon/toutiao.png" width={26} height={26} alt="logo" />
+            </div>
           </div>
+          {/* 博客列表 */}
+          <div className="grow max-w-3xl flex flex-col space-y-10">
+            {blogList.map((item) => {
+              return (
+                <div className="rounded-md shadow-lg shadow-slate-200 px-6 pt-6 pb-4 bg-white">
+                  <Link className="text-2xl font-bold" href="/2023/05/07/helloworld">
+                    只写样式名的 css —— tailwindcss
+                  </Link>
+                  <div className="text-base text-slate-400 my-2.5">2023-8-6 13:14:55</div>
+                  <div className="text-base ">
+                    做梦也不会想到，前端开发里最意会的 css 居然能公式化出来。简简单单的 flex justify-between 就能代替原来书写多行的样式。你知道：text-cyan-700 text-2xl font-bold tracking-widest
+                    什么意思吗？
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="w-60">博客推荐列表</div>
         </div>
         {/* 博客列表 */}
-        <div className="grow max-w-3xl flex flex-col space-y-10">
-          {blogList.map((item) => {
-            return (
-              <div className="rounded-md shadow-lg shadow-slate-200 px-6 pt-6 pb-4 bg-white">
-                <Link className="text-2xl font-bold" href="/2023/05/07/helloworld">
-                  只写样式名的 css —— tailwindcss
-                </Link>
-                <div className="text-base text-slate-400 my-2.5">2023-8-6 13:14:55</div>
-                <div className="text-base ">
-                  做梦也不会想到，前端开发里最意会的 css 居然能公式化出来。简简单单的 flex justify-between 就能代替原来书写多行的样式。你知道：text-cyan-700 text-2xl font-bold tracking-widest
-                  什么意思吗？
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="w-60">博客推荐列表</div>
       </div>
-      {/* 博客列表 */}
     </div>
   );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import "./Banner.css";
 
 export default function Banner() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-4 bg-cover bg-no-repeat bg-fixed bg-center bg-[url('/images/banner.jpeg')] h-96 w-full">
+    <div className="flex flex-col items-center justify-center space-y-4 bg-cover bg-no-repeat bg-fixed bg-center bg-[url('/images/banner.jpeg')] h-[60vh] w-full">
       {/* <img src="/images/banner.jpeg" className="scale-50" width="100%" alt="Picture of the author" /> */}
       <h2 className="text-cyan-700 text-4xl font-bold tracking-wider">前端雨爸的博客</h2>
       <h3 className="text-cyan-700 text-2xl font-bold tracking-widest">与其草率敷衍，不如深究到底</h3>
